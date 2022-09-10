86b026fe9183657e9a1c11f4cf74e7fb62eb076c
kawaguchi880 <yaoshengchuankou1@gmail.com>
2022-09-10 05:26:00 +0000

    first commit

---
+++ app.py
@@ -0,0 +1,15 @@
+from datetime import datetime
+from flask import Flask, render_template, request, redirect, url_for, send_from_directory
+app = Flask(__name__)
+
+
+@app.route('/')
+def index():
+   print('Request for index page received')
+   return render_template('index.html')
+
+
+
+
+if __name__ == '__main__':
+   app.run()
