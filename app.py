332271b05b87d84e5c8637ab40fad53a7a88044b
kawaguchi880 <yaoshengchuankou1@gmail.com>
2022-09-29 17:16:16 +0000

    update mypage

--- app.py
+++ app.py
@@ -1,15 +1,17 @@
 from datetime import datetime
-from flask import Flask, render_template, request, redirect, url_for, send_from_directory
+from flask import Flask, render_template, request, redirect, url_for, send_from_directory,make_response
+import json
 
 app = Flask(__name__)
-
-      
 
 
 @app.route('/')
 def index():
-   print('Request for index page received')
-   return render_template('index.html')
+   user_info = request.cookies.get('user_info')
+   if user_info is not None:
+        user_info = json.loads(user_info)
+   return render_template('index.html',user_info=user_info)
+
 
 
 @app.route('/minesweeper_easy')
@@ -28,7 +30,22 @@ def minesweeper_hard():
 def mypage():
    return render_template('mypage.html')
 
+@app.route('/setcookie',methods = ['POST','GET'])
+def setcookie():
+   if request.method =='POST':
+      user_name = request.form['nm']
+      user_icon = request.form['icon']
+      user_info = {'user_name':user_name,'user_icon':user_icon}
 
+   res = make_response(render_template('mypage.html'))
+   res.set_cookie('user_info',value=json.dumps(user_info))
+   return res
+
+@app.route('/delete_cookie')
+def delete_cookie():
+    res = make_response(render_template('mypage.html'))
+    res.delete_cookie('user_info')
+    return res
 
 
 if __name__ == '__main__':
